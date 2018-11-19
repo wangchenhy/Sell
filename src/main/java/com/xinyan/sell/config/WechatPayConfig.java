bcc4d142b9ed8649493b1ecb76c609c76051af1d
王宸 <2296122219@qq.com>
2018-11-19 15:37:35 +0800

    15:37 微信页面

---
+++ src/main/java/com/xinyan/sell/config/WechatPayConfig.java
@@ -0,0 +1,34 @@
+package com.xinyan.sell.config;
+
+import com.lly835.bestpay.config.WxPayH5Config;
+import com.lly835.bestpay.service.BestPayService;
+import com.lly835.bestpay.service.impl.BestPayServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 支付配置
+ */
+@Configuration
+public class WechatPayConfig {
+
+    @Autowired
+    private WechatAccountConfig wechatAccountConfig;
+
+    @Bean
+    public BestPayService bestPayService(){
+        WxPayH5Config wxPayH5Config = new WxPayH5Config();
+        wxPayH5Config.setAppId(wechatAccountConfig.getMpAppId());
+        wxPayH5Config.setAppSecret(wechatAccountConfig.getMpAppSecret());
+        wxPayH5Config.setMchId(wechatAccountConfig.getMchId());
+        wxPayH5Config.setMchKey(wechatAccountConfig.getMchKey());
+        wxPayH5Config.setKeyPath(wechatAccountConfig.getKeyPath());
+        wxPayH5Config.setNotifyUrl(wechatAccountConfig.getNotifyUrl());
+
+        BestPayServiceImpl bestPayService = new BestPayServiceImpl();
+        bestPayService.setWxPayH5Config(wxPayH5Config);
+
+        return bestPayService;
+    }
+}
